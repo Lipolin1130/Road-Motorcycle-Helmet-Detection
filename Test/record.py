b84e2999d3b756176c7e7b53d71667980a24b102
Lipolin1130 <funghi1130@gmail.com>
2023-11-21 21:40:33 +0800

    change record.py

--- Test/record.py
+++ Test/record.py
@@ -3,11 +3,9 @@ import datetime
 import time
 
 # 設定連結
-video_url = "https://cctv.bote.gov.taipei:8501/MJPEG/157?t=0.4947345277427502"
+video_url = "https://cctv.bote.gov.taipei:8501/MJPEG/157"
 
-recording_duration = 15 * 60  # 15分鐘
-
-wait_time = 44 * 60
+recording_duration = 1 * 60 * 3  # 15分鐘
 
 # 設定影片存儲資料夾
 output_folder = "D:\\Project\\Road Motorcycle Helmet Detection\\Test\\videos"
@@ -16,43 +14,59 @@ while True:
 
     current_time = datetime.datetime.now()
     
-    if current_time.minute == 0 and current_time.second == 0:
-        current_date = current_time.strftime("%Y_%m_%d")
-        current_time_str = current_time.strftime("%H_%M_%S")
+    # if current_time.minute == 0 and current_time.second == 0:
+    current_date = current_time.strftime("%Y_%m_%d")
+    current_time_str = current_time.strftime("%H_%M_%S")
 
-        # 獲取當前日期和時間
-        # 設定影片檔名
-        video_filename = f"{current_date}_{current_time_str}.mp4"
+    # 獲取當前日期和時間
+    # 設定影片檔名
+    video_filename = f"{current_date}_{current_time_str}.mp4"
 
-        # 設定影片儲存路徑
-        output_path = f"{output_folder}/{video_filename}"
+    # 設定影片儲存路徑
+    output_path = f"{output_folder}/{video_filename}"
 
-        # 創建VideoCapture對象
-        cap = cv2.VideoCapture(video_url)
+    # 創建VideoCapture對象
+    cap = cv2.VideoCapture(video_url)
 
-        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-        # 創建VideoWriter對象
-        fourcc = cv2.VideoWriter_fourcc(*'MTPG')
-        out = cv2.VideoWriter(output_path, fourcc, 20.0, (width, height))
-        print("Start recording...")
-        # 開始錄製
-        start_time = time.time()
-        while time.time() - start_time < recording_duration:
-            ret, frame = cap.read()
-            if not ret:
-                print('Cannot receive frame')
-                break
-            out.write(frame)
-            cv2.imshow("Recording", frame)  # 顯示當前捕獲的幀
-            if cv2.waitKey(1) & 0XFF == ord(' '): # 等待1毫秒
-                break
-            
-        # 釋放資源
-        cap.release()
-        out.release()
-        cv2.destroyAllWindows()
+    # 創建VideoWriter對象
+    fourcc = cv2.VideoWriter_fourcc(*'MTPG')
+    out = cv2.VideoWriter(output_path, fourcc, 20.0, (width, height))
+    print("Start recording...")
+    # 開始錄製
+    start_time = time.time()
+    time_lost_start = 0
+    time_lost = 0
+    print("start_time: ", start_time)
+    while time.time() - start_time <= recording_duration:
+        ret, frame = cap.read()
         
-        print('Finish recording...' + video_filename)
-        time.sleep(wait_time)
+        if time_lost_start == 1:
+            time_lost_start = 0
+            recording_duration += (time.time() - time_lost) * 3
+            print("recording_duration: ", recording_duration)
+            
+        if not ret:
+            if time_lost_start == 0:
+                time_lost_start = 1
+                time_lost = time.time()
+            print("Connection lost, reconnecting...")
+            cap.release()
+            time.sleep(0.1)
+            cap = cv2.VideoCapture(video_url)
+            continue
+        
+        
+        out.write(frame)
+        cv2.imshow("Recording", frame)  # 顯示當前捕獲的幀
+        if cv2.waitKey(1) & 0XFF == ord(' '): # 等待1毫秒
+            break
+    print("time.time() ", time.time())
+    # 釋放資源
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+    
+    print('Finish recording...' + video_filename)
